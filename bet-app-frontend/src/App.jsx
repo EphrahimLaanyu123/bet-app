ad38d9e36b4c1925f827a25d0df37f85230a71e8
EphrahimLaanyu <elaanyu@gmail.com>
2024-11-25 09:47:20 +0300

    Added odds

---
+++ bet-app-frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import OddsViewer from './bett/OddsViewer';
+import OddsData from '/home/user/bet-app/bet-app-frontend/src/bett/OddsData.jsx';
+function App() {
+  return (
+    <div className="App">
+      <OddsData></OddsData>
+    </div>
+  );
+}
+
+export default App;
