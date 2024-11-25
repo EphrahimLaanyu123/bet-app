f567f7a8ad05fe1633a52f09bc7e49f39eeab873
EphrahimLaanyu <elaanyu@gmail.com>
2024-11-25 09:55:56 +0300

    Corrected error

--- bet-app-frontend/src/App.jsx
+++ bet-app-frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import OddsViewer from './bett/OddsViewer';
-import OddsData from '/home/user/bet-app/bet-app-frontend/src/bett/OddsData.jsx';
+import OddsData from './bett/OddsData'
 function App() {
   return (
     <div className="App">
